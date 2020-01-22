00dd6eef0b17be5ebea8a946d675e15dcf0abb80
moby2006 <60179427+moby2006@users.noreply.github.com>
2020-01-22 15:34:32 +0100

    Code für de_bbavatar_1.3.0

--- Sitesplat/de_bbavatar_1.3.0/language/de/titlesandavatars.php
+++ Sitesplat/de_bbavatar_1.3.0/language/de/titlesandavatars.php
@@ -1,1 +1,25 @@
-titlesandavatars.php
+<?php
+/**
+*
+* BBAvatars
+*
+* @copyright (c) 2015 SiteSplat All rights reserved
+* @license Proprietary
+*
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+    'BBAVATARS_PARTECIPANTS'	=> 'Thema Mitwirkende',
+	'PARTICIPATE_OTHERS'	    => 'Alle Mitwirkenden anzeigen',
+));
+

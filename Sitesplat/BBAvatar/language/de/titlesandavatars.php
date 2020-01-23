9e73231ec40c40890a6e103277ede32a4d4ae984
moby2006 <60179427+moby2006@users.noreply.github.com>
2020-01-23 13:53:29 +0100

    language/de/titlesandavatars.php erstellt

--- Sitesplat/BBAvatar/language/de/titlesandavatars.php
+++ Sitesplat/BBAvatar/language/de/titlesandavatars.php
@@ -1,1 +1,24 @@
-titlesandavatars.php
+<?php
+/**
+*
+* BBAvatars
+*
+* @copyright (c) 2015 SiteSplat All rights reserved
+* @license Proprietary
+*
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+    'BBAVATARS_PARTECIPANTS'	=> 'Themen Mitwirkende',
+	'PARTICIPATE_OTHERS'	    => 'Alle Mitwirkende anzeigen',
+));
